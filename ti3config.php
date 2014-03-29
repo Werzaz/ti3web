d67d2f9226cbbbfd1c05c3fc3661ee216db8c4a9
Werzaz <ulrichfeindt@yahoo.de>
2014-03-29 17:27:06 +0100

    commit ti3config file

---
+++ ti3config.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * ti3constants.php
+ * 
+ * Copyright 2014 Ulrich Feindt <sunchaser@ti3pbf.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+$lock_timeout = 300; /* Time to map lock time out in seconds. */
+$ti3web_url = 'http://ti3pbf.com/ti3web/';
+$ti3web_version = '0.1.1';
+$ti3web_admin = 'Sunchaser';
+$github_link = 'https://github.com/Werzaz/ti3web';
+$thread_link = 'http://ti3pbf.com/board/viewtopic.php?f=14&t=462';
+
+$footer_text = 'TI3Web Map Viewer version ' . $ti3web_version . '<br>
+Source code available on <a href="' . $github_link . '">github</a>. This version is for testing purposes. If you have found a bug, have suggestion or wish to contribute to this project, please post in <a href="' . $thread_link . '">this thread</a> or report issues on github.<br><br> 
+
+<b>Copyright and Trademark Information:</b> Twilight Imperium 3rd Edition is a trademark of Fantasy Flight Publishing, Inc. in the United States and other countries. &copy; 2014 Fantasy Flight Publishing, Inc. All rights reserved.<br><br>
+
+        <b>Legal Notice:</b> This website is not affiliated with, endorsed, sponsored, or specifically approved by Fantasy Flight Publishing, Inc. For more information about Fantasy Flight Publishing, Inc. or any of their trademarks or other intellectual property, please visit <a href="http://www.fantasyflightgames.com">their website</a>.<br><br>
+
+<a href="http://ti3pbf.com/wiki/TI3_PBF_Wiki:About#Impressum">Impressum</a/>';
+
